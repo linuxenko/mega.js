8830a944fb8d88187d48de9db3e6c9cbf6792e8a
Svetlana Linuxenko <linuxenko@yahoo.com>
2017-07-27 23:15:53 +0300

    Port of prepare_key

--- crypto.js
+++ crypto.js
@@ -2,11 +2,42 @@
  * General mega crypto port
  */
 
+var sjcl = require('sjcl');
+var Aes = sjcl.cipher.aes;
+
 // string to array of 32-bit words (big endian)
-module.exports.str_to_a32 = function (b) {
+var str2a32 = function (b) {
   var a = Array((b.length + 3) >> 2);
   for (var i = 0; i < b.length; i++) {
     a[i >> 2] |= (b.charCodeAt(i) << (24 - (i & 3) * 8));
   }
   return a;
 };
+
+// convert user-supplied password array
+var prepareKey = function (a) {
+  var i, j, r, key;
+  var aes = [];
+  var pkey = [0x93C467E3, 0x7DB0C7A4, 0xD1BE3F81, 0x0152CB56];
+
+  for (j = 0; j < a.length; j += 4) {
+    key = [0, 0, 0, 0];
+    for (i = 0; i < 4; i++) {
+      if (i + j < a.length) {
+        key[i] = a[i + j];
+      }
+    }
+    aes.push(new Aes(key));
+  }
+
+  for (r = 65536; r--;) {
+    for (j = 0; j < aes.length; j++) {
+      pkey = aes[j].encrypt(pkey);
+    }
+  }
+
+  return pkey;
+};
+
+module.exports.str_to_a32 = str2a32;
+module.exports.prepare_key = prepareKey;
--- tests/crypto.test.js
+++ tests/crypto.test.js
@@ -6,4 +6,10 @@ describe('Test crypto.js functions', function () {
     expect(crypt.str_to_a32('123')).deep.equal([ 825373440 ]);
     expect(crypt.str_to_a32('string')).deep.equal([ 1937011305, 1852243968 ]);
   });
+
+  it('should create prepare_key fn', function () {
+    expect(crypt.prepare_key).to.be.a('function');
+    expect(crypt.prepare_key(crypt.str_to_a32('string'))).deep.equal(
+      [ 620029680, 945057733, -2051893932, 299760322 ]);
+  });
 });
